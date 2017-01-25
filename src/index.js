81de9569f8a8ce4264063ba1ccb2ecd91d21320f
joshgressman <joshgressman@gmail.com>
2017-01-24 20:34:32 -0600

    countdownApp files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'; // allows you to use react and all of the components
+import ReactDOM from 'react-dom'; // controls react code on the browser
+
+ReactDOM.render(
+<div><h1>countdownApp</h1></div>, document.getElementById('root') //targets id in html displays to DOM
+);
