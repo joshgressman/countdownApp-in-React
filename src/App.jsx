00af739e3a112498d421b04a729ca6c3eaab3425
joshgressman <joshgressman@gmail.com>
2017-01-25 20:01:02 -0600

    added functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import React, { Component} from 'react';
 import './App.css';
 //component is a reusable piece on the ui
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      deadline: 'December 25, 2017'
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: 'October 25, 2017' }) //declairing an object to set state
+  }
+
 render() {
   return (
     <div className = 'App'>
-      <div className = 'App-title' >Countdown to December 25th, 2017 </div>
+      <div className = 'App-title' >Countdown to {this.state.deadline}</div>
       <div>
         <div className ="Clock-days">14 days</div>
         <div className ="Clock-hours">30 hours</div>
@@ -13,7 +24,7 @@ render() {
         <div className ="Clock-sec">20 seconds</div>
       </div>
       <input placeholder ='new date' />
-      <button>Submit</button>
+      <button onClick={() => this.changeDeadline()}>Submit</button>
     </div>
   )
   }
