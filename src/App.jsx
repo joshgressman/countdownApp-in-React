e9149066e52e6832b8165b6a38fa06e656e6e31f
joshgressman <joshgressman@gmail.com>
2017-01-26 18:59:13 -0600

    added funciton to input

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      deadline: 'December 25, 2017'
+      deadline: 'December 25, 2017',
+      newDeadline: ''
     }
   }
 
   changeDeadline() {
-    this.setState({deadline: 'October 25, 2017' }) //declairing an object to set state
+      this.setState({deadline: this.state.newDeadline})
   }
 
 render() {
   return (
-    <div className = 'App'>
-      <div className = 'App-title' >Countdown to {this.state.deadline}</div>
+    <div className='App'>
+      <div className='App-title' >Countdown to {this.state.deadline}</div>
       <div>
-        <div className ="Clock-days">14 days</div>
-        <div className ="Clock-hours">30 hours</div>
-        <div className ="Clock-min">15 minutes</div>
-        <div className ="Clock-sec">20 seconds</div>
+        <div className="Clock-days">14 days</div>
+        <div className="Clock-hours">30 hours</div>
+        <div className="Clock-min">15 minutes</div>
+        <div className="Clock-sec">20 seconds</div>
       </div>
-      <input placeholder ='new date' />
+      <input placeholder='new date' onChange={event => this.setState({newDeadline: event.target.value})}/>
       <button onClick={() => this.changeDeadline()}>Submit</button>
     </div>
   )
